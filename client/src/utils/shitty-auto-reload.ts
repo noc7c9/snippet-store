048156d1b7112e57aa36f0e93fea9de373cd56bc
Athir Saleem <noc7c9@gmail.com>
2020-09-05 13:07:44 +1000

    Ensure auto reload does not trigger in production

--- client/src/utils/shitty-auto-reload.ts
+++ client/src/utils/shitty-auto-reload.ts
@@ -1,18 +1,20 @@
-let hash: null | string = null;
-const shittyReload = async () => {
-    const res = await fetch('/__SHITTY_AUTO_RELOAD__');
-    const data = await res.json();
+if (process.env.NODE_ENV === 'development') {
+    let hash: null | string = null;
+    const shittyReload = async () => {
+        const res = await fetch('/__SHITTY_AUTO_RELOAD__');
+        const data = await res.json();
 
-    if (hash == null) {
-        hash = data.hash;
-    } else if (hash !== data.hash) {
-        location.reload();
-    }
+        if (hash == null) {
+            hash = data.hash;
+        } else if (hash !== data.hash) {
+            location.reload();
+        }
 
-    setTimeout(shittyReload, 500);
-};
+        setTimeout(shittyReload, 500);
+    };
 
-console.log('SHITTY AUTO RELOAD ENABLED');
-shittyReload();
+    console.log('SHITTY AUTO RELOAD ENABLED');
+    shittyReload();
+}
 
 export default undefined;
