66f635365fa46b40e3fa82a8c393b0dbf9eb3d0f
Athir Saleem <athirs@alpaca.travel>
2020-09-06 11:19:47 +1000

    Setup default value for server PORT config

--- server/src/config.ts
+++ server/src/config.ts
@@ -2,7 +2,7 @@ import 'dotenv-safe/config';
 
 export const NODE_ENV = process.env.NODE_ENV;
 
-export const PORT = process.env.PORT;
+export const PORT = process.env.PORT || 3000;
 export const STORAGE = process.env.STORAGE;
 
 export const DYNAMODB_TABLE = process.env.DYNAMODB_TABLE;
