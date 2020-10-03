63ca5d7e9334e04e71e4c0c79fb723d1ea49939d
Athir Saleem <athirs@alpaca.travel>
2020-10-03 19:34:24 +1000

    Remove eslint-disable

--- server/src/storage/index.ts
+++ server/src/storage/index.ts
@@ -27,8 +27,7 @@ else if (config.STORAGE === 'dynamodb') {
 
 // Unknown
 else {
-    // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
-    throw new Error(`Unknown env.STORAGE value: ${config.STORAGE}`);
+    throw new Error(`Unknown env.STORAGE value: ${String(config.STORAGE)}`);
 }
 
 export default storage;
